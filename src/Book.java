5c40846c0d4c440345476a3df5a35c0f4459f159
Ilyin Ilia <ilyin88ok@gmail.com>
2024-03-24 01:19:37 +0430

    Added: toString, equals, hashcode.

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,5 @@
+import java.util.Objects;
+
 public class Book {
     private String bookName;
     private Author author;
@@ -23,5 +25,24 @@ public class Book {
 
     public void setYearPublication(int yearPublication) {
         this.yearPublication = yearPublication;
+    }
+
+    public String toString() {
+        return "Название: " + this.bookName +
+                ", Автор: " + this.author +
+                ", Год издания: " + this.yearPublication;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return yearPublication == book.yearPublication && Objects.equals(bookName, book.bookName) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookName, author, yearPublication);
     }
 }
